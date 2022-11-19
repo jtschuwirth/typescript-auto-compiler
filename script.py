a38ae4a53b8213cbb4e8519c0d39aff720546604
jtschuwirth <jtschuwirth@uc.cl>
2022-11-19 20:54:56 -0300

    only compiles files that have changed

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import os
+import hashlib
+import json
 
 #Find all ts files in the repository
 
@@ -18,11 +20,23 @@ def find_files():
 
 #run tsc file_path/file_name on every ts file
 
-def compile(file):
-    command = f"tsc {file}"
-    os.system(f"{command}")
+def compile(file, hashes):
+    file_hash=""
+    with open(file, "rb") as f:
+        file_hash = hashlib.md5(f.read()).hexdigest()
+    if file in hashes and hashes[file] == file_hash:
+        return
+    else:
+        hashes[file] = file_hash
+        with open("./hashes.json", "w") as json_file:
+            json.dump(hashes, json_file)
+        command = f"tsc {file}"
+        print("compiling; ", file)
+        os.system(f"{command}")
 
-
+hashes = {}
+with open("./hashes.json", "r") as json_file:
+    hashes = json.load(json_file)
 files = find_files()
 for file in files:
-    compile(file)
+    compile(file, hashes)
